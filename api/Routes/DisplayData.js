a3d09cd628741458b0f10f98d2e2d703f5f3a629
yash <ymandlekar9826@gmail.com>
2023-03-26 15:02:26 +0530

    updating

--- api/Routes/DisplayData.js
+++ api/Routes/DisplayData.js
@@ -5,7 +5,7 @@ router.get("/fooddata", async (req, res) => {
   try {
     const data = await mongoose.connection.db.collection("food_items").find().toArray();
     const data1 = await mongoose.connection.db.collection("food_category").find().toArray();
-    res.json({ items: data, category: data1 });
+    res.json({ items: [], category: data1 });
   } catch (error) {
     console.log(error);
     res.send("server error");
